3a733bd713fd882f5c5daaeb41cb587c283082e1
Ofir Yemini <ofiry1981@gmail.com>
2024-12-25 16:35:52 +0200

    rename client

---
+++ Game.Client/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using GameClient;
+using GameClient.Domain;
+using GameClient.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile("appsettings.json");
+        config.AddEnvironmentVariables();
+    })
+    .UseSerilog((hostingContext, loggerConfig) =>
+    {
+        loggerConfig.ReadFrom.Configuration(hostingContext.Configuration);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddSingleton<IGameServerWs, GameServerWs>();
+        services.AddHostedService<ConsoleMenu>();
+    })
+    .Build();
+
+await host.RunAsync();
+
+// var client = new WebSocketClient("ws://localhost:5214/ws");
+//
+// await client.ConnectAsync();
+//
+//
+// // Send a login request
+// await client.SendLoginRequestAsync("550e8400-e29b-41d4-a716-446655440000");
+// await client.ListenForResponsesAsync();
+// // Simulate a delay, then send an update request
+// await Task.Delay(2000);
+// await client.SendUpdateRequestAsync(ResourceType.Coins, 100);
+// await client.ListenForResponsesAsync();
--- Game.Contracts/MessageType.cs
+++ Game.Contracts/MessageType.cs
@@ -3,7 +3,7 @@
 public enum MessageType:byte
 {
     LoginRequest = 1,
-    LoginResponse = 2,
+    LoginResponse = 2,//todo:remove
     UpdateRequest = 3,
     SendGift = 4,
 }
